86165df3220aaba2d2a36403da18f581a27dff20
カイマク <kaimaku.inc@gmail.com>
2022-01-15 09:27:58 +0900

    problemのコンポーネントを作成

--- src/components/firstLook.tsx
+++ src/components/firstLook.tsx
@@ -1,18 +1,26 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import toAppstore from '../../public/images/toAppstore.png';
-import layer1 from '../../public/images/layer1.png';
-import layer2 from '../../public/images/layer2.png';
-import layer3 from '../../public/images/layer3.png';
-import backgroundImage from '../../public/images/backgroundImage.png';
+import Layer1 from '../../public/images/layer1.png';
+import Layer2 from '../../public/images/layer2.png';
+import Layer3 from '../../public/images/layer3.png';
+import BackgroundBaseImage from '../../public/images/backgroundBaseImage.png';
+import LogoImage from '../../public/images/logoImage.png';
+import LogoText from '../../public/images/logoText.png';
 
 const FirstLook: NextPage = () => {
   return (
     <>
-      <div className="pt-16 bg-blue-200">
-        <div className="flex flex-row px-36">
-          <div className="flex flex-col mr-12 ml-12 max-w-3xl">
-            <a href="#">カイマクロゴ</a>
+      <div className="mb-40">
+        <Image src={BackgroundBaseImage} width={1600} height={600} className="z-0 w-full" />
+        <div className="flex flex-row px-36 absolute top-32">
+          <div className="flex flex-col mx-12 max-w-3xl">
+            <a href="#" className="-mt-12">
+              <div className="flex flex-row gap-2">
+                <Image src={LogoImage} alt="Logo Image" />
+                <Image src={LogoText} alt="Logo Text" />
+              </div>
+            </a>
             <div>
               <p className="text-[52px] font-bold whitespace-nowrap mt-32">
                 大学生活で
@@ -21,7 +29,7 @@ const FirstLook: NextPage = () => {
                 <br />
                 頑張ってみたいあなたへ
               </p>
-              <p className="text-[24px] whitespace-nowrap">
+              <p className="text-[24px] whitespace-nowrap py-8">
                 学生と企業をつなぐ長期インターンマッチングアプリ
               </p>
               <a href="#" className="-ml-4">
@@ -30,18 +38,21 @@ const FirstLook: NextPage = () => {
             </div>
           </div>
           <div className="relative w-full -ml-20">
-            <div className="absolute left-32 top-16">
-              <Image src={layer1} width={380} height={454} className="z-30 absolute" />
+            <div className="absolute -right-12 top-16">
+              <Image src={Layer1} width={380} height={454} className="z-30 absolute" />
             </div>
             <div className="absolute top-4 left-16">
-              <Image src={layer2} width={440} height={596} className="z-20 absolute" />
+              <Image src={Layer2} width={440} height={596} className="z-20 absolute" />
             </div>
             <div className="absolute top-12">
-              <Image src={layer3} width={480} height={560} className="z-10 absolute" />
+              <Image src={Layer3} width={480} height={560} className="z-10 absolute" />
             </div>
 
-            {/* 一応写っているもののわかりずらい */}
-            <Image src={backgroundImage} width={653} height={653} className="absolute z-0" />
+            <div className="relative border border-white rounded-full w-[462px] h-[462px] left-36 -top-12 opacity-20">
+              <div className="absolute border border-white rounded-full w-[320px] h-[320px] top-[70px] left-[70px]"></div>
+              <div className="absolute border border-white rounded-full w-[211px] h-[211px] top-[125px] left-[125px]"></div>
+              <div className="absolute border border-white rounded-full w-[160px] h-[160px] top-[150px] left-[150px]"></div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/problem.tsx
+++ src/components/problem.tsx
@@ -1,15 +1,59 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import ProblemTitle from '../../public/images/problemTitle.png';
+import ProblemPeople from '../../public/images/problemHuman.png';
+import CloserVector from '../../public/images/closerVector.png';
+import FarVector from '../../public/images/farVector.png';
+import HugeVector1 from '../../public/images/hugeVector1.png';
+import HugeVector2 from '../../public/images/hugeVector2.png';
+import HugeVector3 from '../../public/images/hugeVector3.png';
 
 const Problem: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col px-36">
-        <div>
-          <Image src={ProblemTitle} />
+      <div>
+        <div className="flex flex-col px-36 py-36">
+          <div className="mx-auto w-fit">
+            <Image src={ProblemTitle} alt="PROBLEM" className="" />
+          </div>
+          <p className="text-center text-[30px] font-semibold tracking-widest pt-4">
+            こんな悩みありませんか？
+          </p>
         </div>
-        <div></div>
+
+        <div className="flex flex-row">
+          <div className="pt-32">
+            <div className="w-96 relative left-52">
+              <Image src={ProblemPeople} alt="PROBLEM PEOPLE" />
+              <div className="absolute w-96 left-[300px] -top-8">
+                <Image src={CloserVector} alt="CLOSER VECTOR" />
+              </div>
+              <div className="absolute w-96 left-[350px] -top-16">
+                <Image src={FarVector} alt="FAR VECTOR" />
+              </div>
+              <div>
+                <div className="absolute w-96 left-[385px] -top-52">
+                  <Image src={HugeVector1} alt="HUGE VECTOR 1" className="relative z-0" />
+                  <p className="font-bold absolute z-10 left-[26px] top-24">
+                    なんとなく<span className="text-blue-400">将来が不安</span>だな...
+                  </p>
+                </div>
+                <div className="absolute w-96 left-[700px] -top-52">
+                  <Image src={HugeVector2} alt="HUGE VECTOR 2" className="relative z-0" />
+                  <p className="font-bold absolute z-10 left-[28px] top-[105px]">
+                    <span className="text-blue-400">ありきたりなガクチカ</span>になるかも...
+                  </p>
+                </div>
+                <div className="absolute w-96 left-[500px] top-0">
+                  <Image src={HugeVector3} alt="HUGE VECTOR 3" className="relative z-0" />
+                  <p className="font-bold absolute z-10 left-[28px] top-[92px]">
+                    友達よりも<span className="text-blue-400">実力をつけたい</span>な...
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
